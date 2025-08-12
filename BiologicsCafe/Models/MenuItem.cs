fc6939be40877674251c7324b6c09c8d76940889
vikass_igt <vikass@infoglobaltech.com>
2025-08-12 08:02:31 +0530

    MenuItem and OrderItem model code added

---
+++ BiologicsCafe/Models/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BiologicsCafe.Models
+{
+
+    public enum ItemType { Food, Drink }
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BiologicsCafe/Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BiologicsCafe.Models
+{
+    public class OrderItem
+    {
+        public MenuItem Item { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal TotalPrice => Item.Price * Quantity;
+    }
+}

8c2063841298a61bc54fa4c597e952b41e898a30
vikass_igt <vikass@infoglobaltech.com>
2025-08-12 08:40:27 +0530

    Created order service there is added code for add new order, list of order items, find subtotal

---
+++ BiologicsCafe/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using BiologicsCafe.Models;
+
+namespace BiologicsCafe.Services
+{
+    public class OrderService
+    {
+        private readonly List<OrderItem> _orderItems;
+        public OrderService()
+        {
+            _orderItems = new List<OrderItem>();
+        }
+
+        public void AddOrder(MenuItem item, int quantity)
+        {
+            var orderItem = _orderItems.FirstOrDefault(o => o.Item.Name == item.Name);
+            if(orderItem != null)
+                orderItem.Quantity += quantity;
+            else
+                _orderItems.Add(new OrderItem { Item = item, Quantity = quantity});
+        }
+
+        public List<OrderItem> GetOrderItems() => _orderItems;
+
+        public decimal GetSubtotal() => _orderItems.Sum(o => o.TotalPrice);
+    }
+}
